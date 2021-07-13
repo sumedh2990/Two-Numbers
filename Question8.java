e8c4352f3103188618a44e583799659beef1f996
Sumedh T Bhalerao <60027999+sumedh2990@users.noreply.github.com>
2021-07-13 14:42:39 +0600

    Add files via upload

---
+++ Question8.java
@@ -0,0 +1,33 @@
+package ComprinnoTest;
+
+import java.util.Scanner;
+
+public class Question8 {
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		  Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+         int a;
+         int b;
+         int n;
+         int min;
+         int max;
+        for(int i=0;i<t;i++){
+            a=sc.nextInt();
+            b=sc.nextInt();
+            n=sc.nextInt();
+            
+                   if(n%2!=0){
+                    a=a*2;    }
+                  
+          
+            
+                max=Math.max(a,b);
+                min=Math.min(a,b);
+               int ans=max/min;
+            System.out.println(ans);
+        }
+           
+
+    }
+}
